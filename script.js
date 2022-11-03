d075e9956dfa17f64616003e549e458dd6a2bdef
luiza-paloschi <luizapaloschi@gmail.com>
2022-11-03 17:55:58 -0300

    Versão 1 com criação de perguntas do quizz

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ let urlImage;
 let questionsNumber;
 let levelsNumber;
 
+let objQuizz = {title: "",
+                image: "",
+                questions: [],
+                levels: []
+                }
+
 //Passar da página inicial para a de criar quizz
 function criarQuizz(){
     const pageListQuizzes = document.querySelector('.page-list-Quizzes');
@@ -31,6 +37,9 @@ function prosseguirPerguntas(){
         questionsNumber = document.getElementById('number-questions').value;
         levelsNumber = document.getElementById('number-levels').value;
       
+        objQuizz.title = titleQuizz;
+        objQuizz.image = urlImage;
+
         const infoBasica = document.querySelector('.basic-info');
         infoBasica.classList.add('esconder');
         const questions = document.querySelector('.questions');
@@ -50,8 +59,8 @@ function renderizarPerguntas(numero){
             <ion-icon class="outline" name="create-outline" onclick="expand(this)"></ion-icon>
         </div>
         <div class ="answers esconder">
-            <input type="text" class="title-question first" placeholder="Texto da pergunta"/>
-            <input type="text" class="color-question" placeholder="Cor de fundo da pergunta"/>
+            <input id="text${[i]}" type="text" class="title-question first" placeholder="Texto da pergunta"/>
+            <input id="color${[i]}" type="text" class="color-question" placeholder="Cor de fundo da pergunta"/>
             <div class="correct">
                 <h2>Resposta correta</h2>
                 <input type="text" class="correct-answer answer" placeholder="Resposta correta"/>
@@ -59,14 +68,14 @@ function renderizarPerguntas(numero){
             </div>
             <div class="incorrect">
                 <h2>Respostas incorretas</h2>
-                <input type="text" class="incorrect-answer1 incorrect" placeholder="Resposta incorreta 1"/>
-                <input type="text" class="url urlIncorrect1" placeholder="URL da imagem 1"/>
+                <input type="text" class="incorrect-answer1 incorrect-answer answer" placeholder="Resposta incorreta 1"/>
+                <input type="text" class="url-incorrect urlIncorrect1" placeholder="URL da imagem 1"/>
 
-                <input type="text" class="incorrect-answer2 icorrect" placeholder="Resposta incorreta 2"/>
-                <input type="text" class="url urlIncorrect2" placeholder="URL da imagem 2"/>
+                <input type="text" class="incorrect-answer2 incorrect-answer answer" placeholder="Resposta incorreta 2"/>
+                <input type="text" class="url-incorrect urlIncorrect2" placeholder="URL da imagem 2"/>
 
-                <input type="text" class="incorrect-answer3 incorrect" placeholder="Resposta incorreta 3"/>
-                <input type="text" class="url urlIncorrect3" placeholder="URL da imagem 3"/>
+                <input type="text" class="incorrect-answer3 incorrect-answer answer" placeholder="Resposta incorreta 3"/>
+                <input type="text" class="url-incorrect urlIncorrect3" placeholder="URL da imagem 3"/>
             </div>
         </div>
     </div>`
@@ -78,17 +87,236 @@ function renderizarPerguntas(numero){
 }
 
 //PAREI AQUI
+let valid;
 function prosseguirNiveis(){
     
+    //Faz a verificação das perguntas
     for(let i =1; i <= questionsNumber; i++){
         const questions = document.querySelector(`.questions .pergunta${[i]}`);
+        if(verifyQuestions(questions)){
+            console.log('ok')
+            valid = true
+        } else{
+            console.log('b')
+            valid = false
+            break;
+        }
+    }
+    //Se estiver tudo certo, adiciona as coisas no objQuizz e vai para a criação de níveis
+    if (valid === true){
+        console.log('allvalid')
+        criarObj();
+        //Ir para a criação de níveis
+        //criarNiveis()
     }
 }
 
-    
-    
+let arrayQuestions = [];
+let objQuestions = {};
+let arrayAnswers = []
+let objAnswers = {}
+let objAnserCorrect={}
+
+function criarObj(){
+    objQuizz.questions = this.addQuestions();
+    console.log(objQuizz)
+}
+
+//Adiciona as perguntas no objQuizz
+function addQuestions(){
+    for(let i =1; i <= questionsNumber; i++){
+        const questions = document.querySelector(`.questions .pergunta${[i]}`);
+        objQuestions.title = document.getElementById(`text${[i]}`).value
+        objQuestions.color = document.getElementById(`color${[i]}`).value
+        objQuestions.answers = addAnswers(questions);
+        arrayQuestions.push(objQuestions);
+        objQuestions = {};
+        objAnswers = {};
+        arrayAnswers = [];
+    }
+   return arrayQuestions;
+}
 
 
+//Adiciona as respostas das questões no objQuizz
+function addAnswers(perguntas){
+   // enquanto tiver respostas na tela na tela para esta pergunta
+
+    const allAvailableAnswers = perguntas.querySelectorAll(".answer")
+    const validAnswers = []
+   
+    for(let i = 0; i < allAvailableAnswers.length; i++){
+        if (allAvailableAnswers[i].value.length !== 0){
+            validAnswers.push(allAvailableAnswers[i])
+        }
+    }
+   console.log(validAnswers.length)
+                            
+   for(let i =1; i <= validAnswers.length; i++){
+     if(i === 1){
+        objAnswers.text = perguntas.querySelector(".correct-answer").value
+        objAnswers.image = perguntas.querySelector(".urlCorrect").value
+        objAnswers.isCorrectAnswer = true
+     } else{
+        objAnswers.text = perguntas.querySelector(`.incorrect-answer${i-1}`).value
+        objAnswers.image = perguntas.querySelector(`.urlIncorrect${i-1}`).value
+        objAnswers.isCorrectAnswer = false
+
+     }
+     arrayAnswers.push(objAnswers);
+     objAnswers={}
+    } 
+
+    return arrayAnswers;
+
+}
+
+
+//Fazer a validação das perguntas
+function verifyQuestions(pergunta){
+    
+    if(!verifyEmptyQuestions(pergunta)){
+        return false
+    }
+     if(!verifyTitleQuestion(pergunta)){
+        return false
+    }
+    if(!verifyColor(pergunta)){
+        return false
+    }
+    if(!verifyCorrectUrl(pergunta)){
+        return false
+    } 
+    if(!verifyIncorrect(pergunta)){
+        return false
+    }
+    if(!verifyFirstIncorrect(pergunta)){
+        return false
+    }  
+    if(!verifySecondIncorrect(pergunta)){
+        return false
+    } 
+    if(!verifyThirdIncorrect(pergunta)){
+        return false
+    } 
+    return true 
+}    
+    
+function verifyEmptyQuestions(pergunta){
+    const text = pergunta.querySelector('.title-question')
+    const color = pergunta.querySelector('.color-question')
+    const correct = pergunta.querySelector('.correct-answer')
+    const urlCorrect = pergunta.querySelector('.urlCorrect')
+    const incorrects = pergunta.querySelectorAll('.incorrect-answer')
+    console.log(incorrects)
+    if (text.value.length === 0 || color.value.length === 0 || correct.value.length === 0 || urlCorrect.value.length === 0){
+        alert('Cada pergunta deve conter: texto, cor, resposta correta e pelo menos 1 resposta incorreta')
+        return false
+    }
+    
+    if (incorrects[0].value.length === 0 && incorrects[1].value.length === 0 && incorrects[2].value.length === 0){
+        alert('Cada pergunta deve conter: texto, cor, resposta correta e pelo menos 1 resposta incorreta')
+        return false
+    } 
+    return true
+}
+
+function verifyTitleQuestion(pergunta){
+    const title = pergunta.querySelector('.title-question')
+    if (title.value.length < 20){
+       alert("O texto da pergunta deve conter no mínimo 20 caracteres")
+       return false
+    }
+    return true
+}
+
+function verifyColor(pergunta){
+    const color = pergunta.querySelector('.color-question').value
+    const regex = /#[0-9A-Fa-f]{6}/g;
+    if (color.match(regex) && color.length <8){
+        console.log ("String is hex");
+        return true
+    }else{
+        console.log("String is not hex");
+        alert("As cores devem estar no formato hexadecimal")
+        return false
+    }
+}
+
+function verifyCorrectUrl(pergunta){
+    const correctUrl = pergunta.querySelector('.urlCorrect').value
+    if(!verifyURL(correctUrl)){
+        return false
+    }
+    return true
+}
+
+function verifyIncorrect(pergunta){
+    const incorrecturls = pergunta.querySelectorAll('.url-incorrect')
+    const incorrects = pergunta.querySelectorAll('.incorrect-answer')
+    if (incorrects[0].value.length === 0 && (incorrects[1].value.length !== 0 || incorrects[2].value.length !== 0)){
+        alert('As respostas incorretas devem ser preenchidas em ordem')
+        return false
+    } 
+    if (incorrecturls[0].value.length === 0 && (incorrecturls[1].value.length !== 0 || incorrecturls[2].value.length !== 0)){
+        alert('As respostas incorretas devem ser preenchidas em ordem')
+        return false
+    } 
+    if (incorrects[1].value.length === 0 && (incorrects[0].value.length !== 0 && incorrects[2].value.length !== 0)){
+        alert('As respostas incorretas devem ser preenchidas em ordem')
+        return false
+    } 
+    if (incorrecturls[1].value.length === 0 && (incorrecturls[0].value.length !== 0 && incorrecturls[2].value.length !== 0)){
+        alert('As respostas incorretas devem ser preenchidas em ordem')
+        return false
+    } 
+    return true
+}
+
+function verifyFirstIncorrect(pergunta){
+   
+    const urlIncorrect1 = pergunta.querySelector('.urlIncorrect1').value
+    
+    if(!verifyURL(urlIncorrect1)){
+        return false
+    }
+    return true
+}
+
+function verifySecondIncorrect(pergunta){
+    const incorrect2 = pergunta.querySelector('.incorrect-answer2')
+    const urlIncorrect2 = pergunta.querySelector('.urlIncorrect2')
+    if (incorrect2.value.length === 0 && urlIncorrect2.value.length === 0){
+        return true
+    }
+    if ((incorrect2.value.length === 0 && urlIncorrect2.value.length !== 0) || 
+    (incorrect2.value.length !== 0 && urlIncorrect2.value.length === 0)){
+        alert("Preencha os campos de resposta corretamente")
+        return false
+    }
+    if(!verifyURL(urlIncorrect2.value)){
+        console.log('aaaaaa')
+        return false
+    }
+    return true
+}
+function verifyThirdIncorrect(pergunta){
+    const incorrect3 = pergunta.querySelector('.incorrect-answer3')
+    const urlIncorrect3 = pergunta.querySelector('.urlIncorrect3')
+    if (incorrect3.value.length === 0 && urlIncorrect3.value.length === 0){
+        return true
+    }
+    if ((incorrect3.value.length === 0 && urlIncorrect3.value.length !== 0) || 
+    (incorrect3.value.length !== 0 && urlIncorrect3.value.length === 0)){
+        alert("Preencha os campos de resposta corretamente")
+        return false
+    }
+    if(!verifyURL(urlIncorrect3.value)){
+        console.log('aaaaaa')
+        return false
+    }
+    return true
+}
 
 
 //Fazer a validadação das informações básicas
